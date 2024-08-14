d22a7afa9d656303242291b9c5c23fe8b0ca3c1a
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-08-13 21:15:06 -0300

    removed function comparePassword at userModel

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,34 +1,26 @@
-import { Schema, model, Document } from "mongoose";
-import bcrypt from "bcrypt";
+import { Schema, model, Document } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
-  role: "admin" | "driver" | "customer";
-  comparePassword(candidatePassword: string): Promise<boolean>;
+  role: 'admin' | 'driver' | 'customer';
 }
 
 const userSchema = new Schema<IUser>({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, enum: ["admin", "driver", "customer"] },
+  role: { type: String, required: true, enum: ['admin', 'driver', 'customer'] },
 });
-
-// Método para comparar senhas
-userSchema.methods.comparePassword = function (
-  candidatePassword: string
-): Promise<boolean> {
-  return bcrypt.compare(candidatePassword, this.password);
-};
 
 // Middleware para criptografar a senha antes de salvar
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) return next();
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, 10);
+//   next();
+// });
 
-export const User = model<IUser>("User", userSchema);
+export const User = model<IUser>('User', userSchema);
