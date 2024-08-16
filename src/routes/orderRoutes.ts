d641c702963aa8764572a3aff3d200b953925939
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-08-16 18:20:34 -0300

    applied jwt verify at order routes

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,19 +1,28 @@
 import { Router } from 'express';
 
 import orderController from '../web/orderController';
+import { authenticateJWT } from '../middlewares/authMiddleware';
 
 const orderRouter = Router();
 //esta ok
-orderRouter.get('/orders', orderController.getOrders);
+orderRouter.get('/orders', authenticateJWT, orderController.getOrders);
 //esta ok
-orderRouter.get('/orders/:id', orderController.getOrderById);
-orderRouter.get('/orders/user/:userName', orderController.getOrderByUser);
-orderRouter.get('/orders/email/:email', orderController.getOrderByEmail);
+orderRouter.get('/orders/:id', authenticateJWT, orderController.getOrderById);
+orderRouter.get(
+  '/orders/user/:userName',
+  authenticateJWT,
+  orderController.getOrderByUser,
+);
+orderRouter.get(
+  '/orders/email/:email',
+  authenticateJWT,
+  orderController.getOrderByEmail,
+);
 //esta ok
-orderRouter.post('/orders', orderController.createOrder);
+orderRouter.post('/orders', authenticateJWT, orderController.createOrder);
 //esta ok
-orderRouter.patch('/orders/:id', orderController.updateOrder);
+orderRouter.patch('/orders/:id', authenticateJWT, orderController.updateOrder);
 
-orderRouter.delete('/orders/:id', orderController.deleteOrder);
+orderRouter.delete('/orders/:id', authenticateJWT, orderController.deleteOrder);
 
 export default orderRouter;
